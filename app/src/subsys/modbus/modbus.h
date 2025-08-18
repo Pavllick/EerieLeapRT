5487faf8202efb241a0b835bc88ef1eab341a6cb
pavlick <pavllick@gmail.com>
2025-08-17 20:12:05 -0400

    Modbus const input buffers + max message size

--- app/src/subsys/modbus/modbus.h
+++ app/src/subsys/modbus/modbus.h
@@ -14,14 +14,14 @@ public:
     Modbus(const char* iface_name);
 
     int Initialize();
-    int ReadCoils(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
-    int ReadDiscreteInputs(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
-    int ReadHoldingRegisters(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
-    int ReadInputRegisters(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
-    int WriteCoil(uint8_t server_id, uint16_t start_address, void* data, const bool value);
+    int ReadCoils(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
+    int ReadDiscreteInputs(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
+    int ReadHoldingRegisters(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
+    int ReadInputRegisters(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
+    int WriteCoil(uint8_t server_id, uint16_t start_address, const void* data, const bool value);
     int WriteHoldingRegister(uint8_t server_id, uint16_t start_address, uint16_t data);
-    int WriteCoils(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
-    int WriteHoldingRegisters(uint8_t server_id, uint16_t start_address, void* data, size_t size_bytes);
+    int WriteCoils(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
+    int WriteHoldingRegisters(uint8_t server_id, uint16_t start_address, const void* data, size_t size_bytes);
 };
 
 }  // namespace eerie_leap::subsys::modbus
