a5883265c7e25e884fa6dc4b9e6d56e85c9af1b1
Pavel Maloletkov <pavllick@gmail.com>
2025-09-07 19:42:06 -0400

    stm32h7 west debug command support added + jlink and dfu-util support

--- boards/mcudev/devebox_stm32h743_hw_20/board.cmake
+++ boards/mcudev/devebox_stm32h743_hw_20/board.cmake
@@ -1,17 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
-# board_runner_args(jlink "--device=STM32H743VI" "--speed=4000")
-
-# include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
-# include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 
-# Doesn't work as expected
-# board_runner_args(stlink_gdbserver "--apid=1")
-# include(${ZEPHYR_BASE}/boards/common/stlink_gdbserver.board.cmake)
+board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
+include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 
-board_runner_args(openocd "--config=${BOARD_DIR}/support/openocd.cfg")
+board_runner_args(openocd --target-handle=_CHIPNAME.cpu0)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+
+board_runner_args(jlink "--device=STM32H743VI" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
